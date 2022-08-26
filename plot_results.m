077c7131370846d153074d6c6b86dce2b27578a2
PinaGruden <62533526+PinaGruden@users.noreply.github.com>
2022-08-26 15:14:32 +0200

    Update plot_results.m

--- plot_results.m
+++ plot_results.m
@@ -6,70 +6,34 @@ switch parameters.signal_type
         Rxy_envelope_ALL_clicks=Rxy_envelope_ALL{1};
         Rxy_envelope_ALL_whistles=Rxy_envelope_ALL{2};
 
-        %% 1) PLOT CROSS-CORRELOGRAM:
-
-        figure;
-        ax1 = axes;
-        im = imagesc(t_serialdate,lags,Rxy_envelope_ALL_clicks); datetick('x','keeplimits');
-        colormap(flipud(gray(256)))
-        ylim([-parameters.d/parameters.c,parameters.d/parameters.c])
-        xlabel('Local Time (HH:MM:SS)'), ylabel('TDOA (s)'),
-        title(['Cross-correlogram based on ', parameters.signal_type])
-        set(gca,'FontSize',12)
-        caxis([0,10])
-        im.AlphaData = 0.5; % change this value to change the background image transparency
-%         axis square;
-        hold all;
-        %plot second data
-        ax2 = axes;
-        im1 = imagesc(t_serialdate,lags,Rxy_envelope_ALL_whistles); datetick('x','keeplimits');
-        colormap(flipud(gray(256)))
-        colorbar
-        ylim([-parameters.d/parameters.c,parameters.d/parameters.c])
-        xlabel('Local Time (HH:MM:SS)'), ylabel('TDOA (s)'),
-        set(gca,'FontSize',12)
-        caxis([0,10])
-        im1.AlphaData = 0.5; % change this value to change the foreground image transparency
-%         axis square;
-        %link axes
-        linkaxes([ax1,ax2])
-        %%Hide the top axes
-        ax2.Visible = 'off';
-        ax2.XTick = [];
-        ax2.YTick = [];
-        set([ax1,ax2],'Position',[.17 .11 .685 .815]);
-        colorbar(ax1,'Position',[.05 .11 .03 .815]);
-        colorbar(ax2,'Position',[.88 .11 .03 .815]);
-%         set(findall(gcf,'type','text'),'FontSize',14)
         
+        
+        %% 1) PLOT Extracted MEASUREMENTS
 
-        %% 2) PLOT TRACKED TDOAS
+        f1=figure(1);
 
-        figure;
-
-        %plot cross-correlograms (overlayed)
-        ax1 = axes;
-        im = imagesc(t_serialdate,lags,Rxy_envelope_ALL_clicks); datetick('x','keeplimits');
+        %plot whistle& click cross-correlograms (overlayed)
+        ax1 = axes(f1);
+        im = imagesc(t_serialdate,lags,Rxy_envelope_ALL_clicks); 
+        datetick('x','keeplimits');
         colormap(flipud(gray(256)))
         ylim([-parameters.d/parameters.c,parameters.d/parameters.c])
         xlabel('Local Time (HH:MM:SS)'), ylabel('TDOA (s)'),
         title(['Cross-correlogram based on ', parameters.signal_type])
-        set(gca,'FontSize',12)
+        set(gca,'FontSize',14)
         caxis([0,10])
         im.AlphaData = 0.5; % change this value to change the background image transparency
-%         axis square;
         hold all;
         %plot second data
-        ax2 = axes;
-        im1 = imagesc(t_serialdate,lags,Rxy_envelope_ALL_whistles); datetick('x','keeplimits');
+        ax2 = axes(f1);
+        im1 = imagesc(t_serialdate,lags,Rxy_envelope_ALL_whistles);
+        datetick('x','keeplimits');
         colormap(flipud(gray(256)))
-        colorbar
         ylim([-parameters.d/parameters.c,parameters.d/parameters.c])
         xlabel('Local Time (HH:MM:SS)'), ylabel('TDOA (s)'),
-        set(gca,'FontSize',12)
+        set(gca,'FontSize',14)
         caxis([0,10])
         im1.AlphaData = 0.5; % change this value to change the foreground image transparency
-%         axis square;
         %link axes
         linkaxes([ax1,ax2])
         %%Hide the top axes
@@ -77,45 +41,111 @@ switch parameters.signal_type
         ax2.XTick = [];
         ax2.YTick = [];
         set([ax1,ax2],'Position',[.17 .11 .685 .815]);
-        colorbar(ax1,'Position',[.05 .11 .03 .815]);
-        colorbar(ax2,'Position',[.88 .11 .03 .815]);
+%         colorbar(ax1,'Position',[.05 .11 .03 .815]);
+%         colorbar(ax2,'Position',[.88 .11 .03 .815]);
        
-        
         %plot measurements
-%         hold on
-%         for k=1:measure.T
-%             if ~isempty(measure.Z{k})
-%                 plot(t_serialdate(k),measure.Z{k}(1,:),'ro')
-%             end
-%         end
-
-        %plot tracked TDOAs
         hold on
-        for k=1:size(Tracks,2)
-            plot(Tracks(k).time_local, Tracks(k).tdoa,'-','LineWidth',3)
+        for k=1:measure.T
+            if ~isempty(measure.Z{k})
+                plot(t_serialdate(k),measure.Z{k}(1,:),'k.'),hold on
+            end
         end
         datetick('x','keeplimits');
 
-%         h(1) = plot(NaN, NaN,'ro','LineWidth',2);
-%         h(2) = plot(NaN, NaN,'b-','LineWidth',2);
-%         legend(h,'Measurements','Tracked TDOAs','Location', 'southeastoutside');
-
-        h(1) = plot(NaN, NaN,'b-','LineWidth',2);
-        legend(h,'Tracked TDOAs','Location', 'southeastoutside');
+        h(1) = plot(NaN, NaN,'k.','LineWidth',4);
+        legend(h,'Measurements','Location', 'southeast');
 
         set(gca,'FontSize',14)
         set(findall(gcf,'type','text'),'FontSize',14)
 
         hold off
 
+        %% 2) PLOT TRACKED TDOAS
+
+        f2=figure(2);
+
+        %plot cross-correlograms (overlayed)
+        ax1 = axes(f2);
+        im = imagesc(t_serialdate,lags,Rxy_envelope_ALL_clicks); 
+        datetick('x','keeplimits');
+        colormap(flipud(gray(256)))
+        ylim([-parameters.d/parameters.c,parameters.d/parameters.c])
+        xlabel('Local Time (HH:MM:SS)'), ylabel('TDOA (s)'),
+        title(['Cross-correlogram based on ', parameters.signal_type])
+        set(gca,'FontSize',14)
+        caxis([0,10])
+        im.AlphaData = 0.5; % change this value to change the background image transparency
+        hold all;
+        %plot second data
+        ax2 = axes(f2);
+        im1 = imagesc(t_serialdate,lags,Rxy_envelope_ALL_whistles); 
+        datetick('x','keeplimits');
+        colormap(flipud(gray(256)))
+        ylim([-parameters.d/parameters.c,parameters.d/parameters.c])
+        xlabel('Local Time (HH:MM:SS)'), ylabel('TDOA (s)'),
+        set(gca,'FontSize',14)
+        caxis([0,10])
+        im1.AlphaData = 0.5; % change this value to change the foreground image transparency
+        %link axes
+        linkaxes([ax1,ax2])
+        %%Hide the top axes
+        ax2.Visible = 'off';
+        ax2.XTick = [];
+        ax2.YTick = [];
+        set([ax1,ax2],'Position',[.17 .11 .685 .815]);
+%         colorbar(ax1,'Position',[.05 .11 .03 .815]);
+%         colorbar(ax2,'Position',[.88 .11 .03 .815]);
+
+        %plot tracked TDOAs
+        hold on
+        for k=1:size(Tracks,2)
+            plot(ax2,Tracks(k).time_local, Tracks(k).tdoa,'-','LineWidth',3), hold on
+        end
+        datetick('x','keeplimits');
+
+        h(1) = plot(NaN, NaN,'b-','LineWidth',3);
+        legend(h,'Tracked TDOAs','Location', 'southeast');
+
+        set(gca,'FontSize',14)
+        set(findall(gcf,'type','text'),'FontSize',14)
+
+        hold off
+
+%% 3) PLOT CROSS-CORRELOGRAMS:
+
+        figure(3);
+
+        subplot(211) %cross-correlogram for clicks
+        imagesc(t_serialdate,lags,Rxy_envelope_ALL_clicks); 
+        datetick('x','keeplimits');
+        colormap(flipud(gray(256)));
+        ylim([-parameters.d/parameters.c,parameters.d/parameters.c]);
+        xlabel('Local Time (HH:MM:SS)'); ylabel('TDOA (s)');
+        title('Cross-correlogram based on clicks');
+        set(gca,'FontSize',14);
+        caxis([0,10]);
+        colorbar;
+
+        subplot(212) %cross-correlogram for whistles
+        imagesc(t_serialdate,lags,Rxy_envelope_ALL_whistles); 
+        datetick('x','keeplimits');
+        colormap(flipud(gray(256)));
+        ylim([-parameters.d/parameters.c,parameters.d/parameters.c]);
+        xlabel('Local Time (HH:MM:SS)'); ylabel('TDOA (s)');
+        title(['Cross-correlogram based on whistles']);
+        set(gca,'FontSize',14);
+        caxis([0,10]);
+        colorbar;
 
 
     otherwise
 
         %% 1) PLOT CROSS-CORRELOGRAM:
 
-        f1= figure;
-        imagesc(t_serialdate,lags,Rxy_envelope_ALL), datetick('x','keeplimits');
+        figure(1);
+        imagesc(t_serialdate,lags,Rxy_envelope_ALL), 
+        datetick('x','keeplimits');
         colormap(flipud(gray(256)))
         caxis([0,10])
         colorbar
@@ -124,15 +154,14 @@ switch parameters.signal_type
         title(['Cross-correlogram based on ', parameters.signal_type])
         set(gca,'FontSize',14)
         set(findall(gcf,'type','text'),'FontSize',14)
-        f1.PaperUnits='centimeters';
-        f1.PaperPosition=[0,0,100,30];%size specified as panorama, 5:1- [left,bottom,width, height]
+        
 
         %% 2) PLOT EXTRACTED MEASUREMENTS
 
-
-        figure;
+        figure(2);
         %plot cross-correlogram
-        imagesc(t_serialdate,lags,Rxy_envelope_ALL), datetick('x','keeplimits');
+        imagesc(t_serialdate,lags,Rxy_envelope_ALL), 
+        datetick('x','keeplimits');
         colormap(flipud(gray(256)))
         caxis([0,10])
         colorbar
@@ -144,12 +173,12 @@ switch parameters.signal_type
         hold on
         for k=1:measure.T
             if ~isempty(measure.Z{k})
-                plot(t_serialdate(k),measure.Z{k}(1,:),'ro')
+                plot(t_serialdate(k),measure.Z{k}(1,:),'k.')
             end
         end
 
-        h(1) = plot(NaN, NaN,'ro','LineWidth',2);
-        legend(h,'Measurements','Location', 'EastOutside');
+        h(1) = plot(NaN, NaN,'k.','LineWidth',4);
+        legend(h,'Measurements','Location', 'southeast');
 
         set(gca,'FontSize',14)
         set(findall(gcf,'type','text'),'FontSize',14)
@@ -158,9 +187,10 @@ switch parameters.signal_type
 
         %% 3) PLOT TRACKED TDOAS
 
-        figure;
+        figure(3);
         %plot cross-correlogram
-        imagesc(t_serialdate,lags,Rxy_envelope_ALL), datetick('x','keeplimits');
+        imagesc(t_serialdate,lags,Rxy_envelope_ALL), 
+        datetick('x','keeplimits');
         colormap(flipud(gray(256)))
         caxis([0,10])
         colorbar
@@ -168,27 +198,15 @@ switch parameters.signal_type
         xlabel('Local Time (HH:MM:SS)'), ylabel('TDOA (s)'),
         title(['Tracked TDOAs based on ', parameters.signal_type])
 
-%         %plot measurements
-%         hold on
-%         for k=1:measure.T
-%             if ~isempty(measure.Z{k})
-%                 plot(t_serialdate(k),measure.Z{k}(1,:),'ro')
-%             end
-%         end
-
         %plot tracked TDOAs
         hold on
         for k=1:size(Tracks,2)
             plot(Tracks(k).time_local, Tracks(k).tdoa,'-','LineWidth',3)
         end
         datetick('x','keeplimits');
-
-%         h(1) = plot(NaN, NaN,'ro','LineWidth',2);
-%         h(2) = plot(NaN, NaN,'b-','LineWidth',2);
-%         legend(h,'Measurements','Tracked TDOAs','Location', 'EastOutside');
  
-        h(1) = plot(NaN, NaN,'b-','LineWidth',2);
-        legend(h,'Tracked TDOAs','Location', 'EastOutside');
+        h(1) = plot(NaN, NaN,'b-','LineWidth',3);
+        legend(h,'Tracked TDOAs','Location', 'southeast');
 
         set(gca,'FontSize',14)
         set(findall(gcf,'type','text'),'FontSize',14)
