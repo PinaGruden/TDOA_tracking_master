04bf645a2fd93ce484c610b4e77b9ab167c00b07
PinaGruden <62533526+PinaGruden@users.noreply.github.com>
2022-08-30 12:25:12 +0200

    Update Specify_Paths.m

--- Specify_Paths.m
+++ Specify_Paths.m
@@ -2,12 +2,21 @@ function [folder, folder2save2] = Specify_Paths
 %CONFIGURE PATHS to recordings & folder to save measurements to:
 
 % Path to Raw data:
-s=what('./Test_example/Data/'); % This is where your .wav files are located
+s=what('./Test_example/Data/wav/'); % This is where your .wav files are located
 folder = [s.path,'/'];
 
-% Path to folder where things will be stored:
-s=what('./Test_example/Results/'); % This is where you want your results to be saved to
-folder2save2= [s.path,'/'];
+% Path to folders where things will be stored:
+% Raw cross-correlogram:
+s=what('./Test_example/Data/Raw_CrossCorrelogram/');
+folder2save2.rawcrosscorr= [s.path,'/'];
+
+% Normalized cross-correlogram and measurements:
+s=what('./Test_example/Data/Measurements/');
+folder2save2.measurements= [s.path,'/'];
+
+% Final tracking results
+s=what('./Test_example/Results/'); % This is where you want your final results to be saved to
+folder2save2.finalresults= [s.path,'/'];
 
 
 end
