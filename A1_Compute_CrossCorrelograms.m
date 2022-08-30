abc309f22f047447f59917e8670a869e6ed2abe5
PinaGruden <62533526+PinaGruden@users.noreply.github.com>
2022-08-30 12:26:49 +0200

    New scripts for TDOA tracking
    New processing scheme where cross-correlogram computation, measurement extraction and tracking is done separately

---
+++ A1_Compute_CrossCorrelograms.m
@@ -0,0 +1,52 @@
+%% COMPUTE CROSS-CORRELOGRAMS
+% A1_Compute_CrossCorrelograms.m is a script that computes
+% cross-correlograms based on two sensors. 
+%!!!!!!!!!!!!!!!!!
+% Make sure you first
+% 1) Specify and change parameters as needed in
+% Specify_Paths.m and Specify_Parameters_CrossCorrelogram.m
+% 2) Specify signal_type variable to be either: 'clicks',
+% 'whistles', or 'both'.
+%!!!!!!!!!!!!!!!!!
+
+addpath('./Preprocess_Extract_Measurements'); 
+clear, close all
+
+
+signal_type ='both';
+
+
+%% 1) USER SPECIFIED PATHS and PARAMETERS:
+
+[folder_data,folder2save2] = Specify_Paths;
+
+switch signal_type
+    case 'clicks'%for clicks:
+        [parameters,parameters_clicks] = Specify_Parameters(signal_type);
+    case 'whistles'% for whistles :
+        [parameters,parameters_whistles] = Specify_Parameters(signal_type);
+    case 'both'
+        [parameters,parameters_clicks,parameters_whistles] = Specify_Parameters(signal_type);
+end
+
+parameters.saveworksp=1; %Save the overall cross-correlogram to folder2save2
+
+%% 2) CROSS-CORRELOGRAM COMPUTATION (and SAVE)
+
+disp('Computing cross-correlograms...')
+switch signal_type
+    case 'clicks'%for clicks:
+        [Rxy_envelope_ALL,lags,t_serialdate,t]=compute_crosscorrelogram(folder_data,folder2save2.rawcrosscorr, parameters,parameters_clicks);
+    case 'whistles'% for whistles :
+        [Rxy_envelope_ALL,lags,t_serialdate,t]=compute_crosscorrelogram(folder_data,folder2save2.rawcrosscorr, parameters,parameters_whistles);
+    case 'both'
+        [Rxy_envelope_ALL_clicks]=compute_crosscorrelogram(folder_data,folder2save2.rawcrosscorr, parameters,parameters_clicks);
+        [Rxy_envelope_ALL_whistles,lags,t_serialdate,t]=compute_crosscorrelogram(folder_data,folder2save2.rawcrosscorr, parameters,parameters_whistles);
+    otherwise
+        disp(['Not a valid signal type. signal_type needs to be either' ...
+             ' "clicks","whistles", or "both". '])
+end
+
+
+
+
---
+++ A3_Track_TDOAs_draft.m
@@ -0,0 +1,52 @@
+%% TRACK TDOAs and DISPLAY RESULTS
+% A3_Track_TDOAs.m is a script that tracks TDOAs based on extracted 
+% measurements from cross-correlograms. 
+%!!!!!!!!!!!!!!!!!
+% IMPORTANT: This assumes that data for one encounter at the time is being
+% processed. This means only one .mat file that contains measurements for
+% the full encounter is in a given folder.
+% Make sure you have first
+% 1) Already run A1_Compute_CrossCorrelograms.m & specified paths in
+% Specify_paths.m (should have been specified before running A1)
+% 2) Already run A2_Extract_Measurements.m
+%!!!!!!!!!!!!!!!!!
+
+
+% - also decide what will get saved
+
+addpath('./GMPHD_SA'); 
+clear, close all
+
+[~,folder2save2] = Specify_Paths;
+
+file = dir(fullfile(folder2save2.measurements,'*.mat' ) );
+load([file.folder,'/',file.name]);
+
+%% TRACK TDOAs:
+disp('Tracking TDOAs from measurements...')
+% Generate tracking models:
+load('BayesOptimization_GMPHDParams_GTchunked_lambda4_5_DiffBirth.mat')
+load('BirthVelocityPrior_AllTrainData.mat')
+[model] = gen_tdoa_tracking_models(parameters, BayesoptResults,birthvelocity);
+
+% Employ GM-PHD-SA to track:
+Est = gmphd_adaptive_amplitude(model,measure); 
+Track = tracktarget_tdoa_labels(Est.Tag,Est.X,model);
+
+% Interpolate and smooth tracks, apply min track length criteria:
+[Tracks] = postprocess(Track,t_serialdate,parameters);
+
+
+%% PLOT RESULTS:
+disp('Plotting results...')
+
+switch parameters.signal_type
+    case 'clicks' 
+        plot_results(t_serialdate,lags,Rxy_envelope_scaled,measure,Tracks, parameters);
+    case 'whistles'
+        plot_results(t_serialdate,lags,Rxy_envelope_scaled,measure,Tracks, parameters);
+    case 'both'
+        plot_results(t_serialdate,lags,{Rxy_envelope_scaled_clicks,Rxy_envelope_scaled_whistles},measure,Tracks, parameters);
+end
+
+%% SAVE
