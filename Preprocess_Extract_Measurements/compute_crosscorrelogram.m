b02b058303f6324727b59e46ba92440d22e7ddbb
PinaGruden <62533526+PinaGruden@users.noreply.github.com>
2024-01-22 19:34:59 -1000

    fixed global time and correction of file timing problem

--- Preprocess_Extract_Measurements/compute_crosscorrelogram.m
+++ Preprocess_Extract_Measurements/compute_crosscorrelogram.m
@@ -103,13 +103,41 @@ starttiming_mismatch(2:end)=seconds(time_start(2:end)-time_end(1:end-1));
 % milliseconds/seconds too late and there is a gap
 
 
-% %-----------------------------------------------------------------
-% % Get GLOBAL TIME vector
-% %-----------------------------------------------------------------
+%-----------------------------------------------------------------
+% Get GLOBAL TIME vector
+%-----------------------------------------------------------------
+find_lowerdt = @(x,y) floor(second(x)/y)*y;
+%function find_lowerdt takes start file datetime information (as x) and
+%time step information in seconds (as y).
+
+find_upperdt = @(x,y) ceil(second(x)/y)*y;
+%function find_upperdt takes start file datetime information (as x) and
+%time step information in seconds (as y).
+
+global_time_start_s= find_lowerdt(time_start(1),dt);%floor(second(time_start(1))/dt)*dt; %these are start seconds
+
+newdatetime= @(x) datetime(year(x), month(x), day(x), hour(x), minute(x),0);
+
+global_time_start= newdatetime(time_start(1))+ seconds(global_time_start_s);
+
+last_file_end=time_start(end)+seconds(duration_file(end));
+
+global_time_end_s= find_lowerdt(last_file_end,dt);%floor(second(last_file_end)/dt)*dt; % end seconds
+
+global_time_end= newdatetime(last_file_end)+ seconds(global_time_end_s);
+
+%start time of each time step from all files in datetime format
+global_time=global_time_start:seconds(dt):global_time_end;
+
+% NOW I will have to modify below to compare each start of the individual
+% files to the global time and either zero pad at the start 
+
 % dur_last=duration_file(end);
 % 
 % t_datetime=time_start(1):seconds(dt):time_start(end)+seconds(dur_last);
 % t_serialdate=datenum(t_datetime);
+t_serialdate=datenum(global_time);
+
 
 %-----------------------------------------------------------------
 % COMPUTE GCC
@@ -131,11 +159,23 @@ if window_length_s < parameters.tau_max
 end
 
 L=min(tau_max_samples*2+1,window_length_samples*2-1);
-M=ceil((seconds(time_start(end)-time_start(1))+duration_file(end))./dt);
+M=length(global_time);
 
 Rxy_envelope_ALL= zeros(L,M);
 
-stop=0;
+indx_timestep_start_k=zeros(N,1);
+indx_timestep_end_k=zeros(N,1);
+
+%Determine time steps where files start and end
+for k = 1:N %iterate through files in the folder
+    indx_timestep_start=find((global_time-time_start(k))<=0); %these are indices of all timesteps before the file starts
+    indx_timestep_start_k(k)=indx_timestep_start(end); %take the last index in the list to be the time step where the file starts
+
+    indx_timestep_end=find((global_time-time_end(k))<=0); %these are indices of all timesteps before the file starts
+    indx_timestep_end_k(k)=indx_timestep_end(end); %take the last index in the list to be the time step where the file starts
+end
+
+
     for k = 1:N %iterate through files in the folder
       
         %% Read the WAV file
@@ -144,28 +184,95 @@ stop=0;
         [x,fs]=audioread([file1(k).folder,'/',file]);
         %Select the two channels to process
         x = x(:,channels);
+
+        % TIME CORECT FILES
+        %Note- this assumes that files only overlap over one time step
+
+        % Check for the beginning of file- if it starts at the same time
+        % step that another file ends OR if it is alone (either first or
+        % after a large gap
+
+        % 1) Get x (input for GCC)
+
+        [L1]=ismember(indx_timestep_start_k(k),indx_timestep_end_k);
+
+        if L1 % the current file starts in the same time step when the previous file ended
+               % the overlap is always sorted out at the end of the file,
+               % so here we just chop off the beginning section that lies
+               % int he overlap.
+
+            % indx_overlap_timestep= indx_timestep_end_k(loc);
+            start_time_read=global_time(indx_timestep_start_k(k)+1); %we want to start reading the file at the next time step (thus +1)
+            start_sample=round(seconds(start_time_read-(time_start(k)))*fs);
+
+            x = x(start_sample:end,:);
+
+           %indicate which time step it starts on:
+            start_timestep=indx_timestep_start_k(k)+1;
+
+        else % this file starts alone in its time step (so it is either a first file or file starting after a large gap ( >=dt or more))
+
+            file_start_delay = seconds(time_start(k) - global_time(indx_timestep_start_k(k))); % delay in start of the file in seconds
+            missing_samples=file_start_delay*fs; % delay in start of the file in samples
+            x = [zeros(missing_samples,size(x,2));x];% zero pad the file
+            % to start at the begininng of the time step
+
+            %indicate which time step it starts on:
+            start_timestep=indx_timestep_start_k(k);
         
-        % CHECK FOR FILE LENGTH and correct if necessary
-        [x,Nstepsmissing] = correct_files(starttiming_mismatch(k),x,fs,dt);
-        start = stop+1+round(Nstepsmissing);
-       
-        %read the next file (one window length of the next file)
-        if k<N
-            [x1,~]=audioread([folder1,'/',file1(k+1).name],[1,window_length_s*fs]);
-            x1 = x1(:,channels);
 
-            % CHECK FOR FILE LENGTH and correct if necessary
-            [x1,Nstepsmissing_x1] = correct_files(starttiming_mismatch(k+1),x1,fs,dt);
+        end
 
-            % If there are no missing steps, ok to read the next file x1,
-            % otherwise one should not
-            if Nstepsmissing_x1>0
-                x1=[];
+        
+        % Check for the END of the current file- if it ends at the same time
+        % step that another file begins OR if it is alone 
+
+        % 2) Get x1 (input for GCC) - this sorts our the end of x array-
+        % the last window of GCC
+
+        [L2]=ismember(indx_timestep_end_k(k),indx_timestep_start_k);
+
+        if L2 % when there is another file starting at the same time step that k-th file ends
+            file2_start_delay = seconds(time_start(k+1) - time_end(k)); % delay in start of the k+1 file relative to the k-th file (in seconds)
+
+
+            if file2_start_delay>=1/fs % the files are non-overlapping
+                %figure out how many samples are in the gap between x and x1 (so start of x1)
+
+                missing_samples2=(file2_start_delay*fs); % delay in start of the file in samples
+                 % missing_samples2=(file2_start_delay*fs)-1;
+
+                %figure out how many samples to read to have a full window:
+                end_time_step=find_upperdt(time_start(k+1),dt); %start of next time step in seconds
+                end_sample= round((end_time_step-second(time_start(k+1))+window_length_s-dt)*fs);
+                % (window_length_s-dt)*fs are missing sample to achieve a
+                % full window for GCC.
+                [x1,~]=audioread([folder1,'/',file1(k+1).name],[1,end_sample]);
+                x1 = x1(:,channels);
+
+                % zero pad the file
+                x1 = [zeros(missing_samples2,size(x1,2));x1];
+
+            else % the files are overlapping
+
+                %figure out how many samples to read to have a full window:
+                end_time_step=find_upperdt(time_start(k+1),dt);
+                end_sample= round((end_time_step-second(time_start(k+1))+window_length_s-dt)*fs);
+                
+                start_sample= round(seconds((time_end(k)) - (time_start(k+1)))*fs);
+                
+                [x1,~]=audioread([folder1,'/',file1(k+1).name],[start_sample,end_sample]);
+                x1 = x1(:,channels);
+
             end
 
-        else %you are reading the last file of the encounter, so there is no file after
+        else % when there is no file starting at the same time step that k-th file ends
             x1=[];
+
         end
+
+
+        stop_timestep=indx_timestep_end_k(k);
           
         %% Specify FILTERS and do FILTERING
         %~~~~~ Bandpass Butterworth ~~~~~~~~~
@@ -196,25 +303,18 @@ stop=0;
         lags=lags(ind);
         RT_envelope = RT_envelope(ind,:);
 
-        [m,M]=size(RT_envelope);
-        stop = start+M-1;
-      
-        Rxy_envelope_ALL(1:m,start:stop) = RT_envelope;
+             
+        Rxy_envelope_ALL(:,start_timestep:stop_timestep) = RT_envelope;
 
 
     end
 
-    %trim away unused pre-allocated space
-    Rxy_envelope_ALL=Rxy_envelope_ALL(1:m,:);
+    % %trim away unused pre-allocated space
+    % Rxy_envelope_ALL=Rxy_envelope_ALL(1:m,:);
 
     t=(0:(size(Rxy_envelope_ALL,2)-1)).*dt; %start times of frames, in seconds (for easier plotting)
 
-    %-----------------------------------------------------------------
-    % Get GLOBAL TIME vector
-    %-----------------------------------------------------------------
-    t_datetime=time_start(1)+seconds(t);
-    t_serialdate=datenum(t_datetime);
-    
+  
     %% SAVE WORKSPACE containing CROSS-CORELOGRAM
     
     if saveworksp==1
